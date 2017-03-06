995b0a7f207b7f17a0774aa92e4f6accabfd4ba7
Jerome Chan <eviltofu@gmail.com>
2017-03-06 09:43:52 +0800

    Removed all but boolean tests

--- core.go
+++ core.go
@@ -4,12 +4,9 @@ import "testing"
 
 // Testing interface to test unit
 type Testing interface {
-	AssertTrue(value bool) Testing
-	AssertFalse(value bool) Testing
-	AssertBoolEqual(expected, value bool) Testing
-	AssertIntEqual(expected, value int) Testing
-	AssertFloatEqual(expected, value float64) Testing
-	AssertStringEqual(expected, value string) Testing
+	AssertTrue(value bool)
+	AssertFalse(value bool)
+	AssertBoolEqual(expected, value bool)
 }
 
 // TestUnit is a test unit
@@ -18,43 +15,18 @@ type TestUnit struct {
 }
 
 // AssertTrue tests if a boolean is true
-func (t TestUnit) AssertTrue(value bool) Testing {
-	return t.AssertBoolEqual(true, value)
+func (t TestUnit) AssertTrue(value bool) {
+	t.AssertBoolEqual(true, value)
 }
 
 // AssertFalse tests if a boolean is false
-func (t TestUnit) AssertFalse(value bool) Testing {
-	return t.AssertBoolEqual(false, value)
+func (t TestUnit) AssertFalse(value bool) {
+	t.AssertBoolEqual(false, value)
 }
 
 // AssertBoolEqual tests if a boolean is equal
-func (t TestUnit) AssertBoolEqual(expected, value bool) Testing {
+func (t TestUnit) AssertBoolEqual(expected, value bool) {
 	if value != expected {
 		t.T.Errorf("Expected %t but got %t", expected, value)
 	}
-	return t
-}
-
-// AssertIntEqual tests if ints are equal
-func (t TestUnit) AssertIntEqual(expected, value int) Testing {
-	if value != expected {
-		t.T.Errorf("Expected %d but got %d", expected, value)
-	}
-	return t
-}
-
-// AssertStringEqual tests if strings are equal
-func (t TestUnit) AssertStringEqual(expected, value string) Testing {
-	if value != expected {
-		t.T.Errorf("Expected %s but got %s", expected, value)
-	}
-	return t
-}
-
-// AssertFloatEqual tests if float64 are equal
-func (t TestUnit) AssertFloatEqual(expected, value float64) Testing {
-	if value != expected {
-		t.T.Errorf("Expected %f but got %f", expected, value)
-	}
-	return t
 }
